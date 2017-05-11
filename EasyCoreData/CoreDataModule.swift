4b0179ecba4f765836e532f8c061f5866cad5441
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2017-05-11 14:56:46 -0700

    Create Module Class

--- EasyCoreData/CoreDataModule.swift
+++ EasyCoreData/CoreDataModule.swift
@@ -9,93 +9,17 @@
 import UIKit
 import CoreData
 
-public class CoreDataModule {
+public class CoreDataModule: Module {
     
     private var entity: String!
-    private var name: String!
-
-    public init(entityName: String, appName: String) {
+    
+    public init(entityName: String) {
         entity = entityName
-        name = appName.replacingOccurrences(of: " ", with: "_")
     }
     
-    
-    private func managedObjectModel() -> NSManagedObjectModel  {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = Bundle.main.url(forResource: name!, withExtension: "momd")!
+    public func push<V>(values: [V], keys: [String]) {
         
-        return NSManagedObjectModel(contentsOf: modelURL)!
-    }
-    
-    private func persistentContainer() -> NSPersistentContainer {
-        let container = NSPersistentContainer(name: name!)
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-            
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
         
-    }
-    
-    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel())
-        //        let directory = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "")
-        
-        let url = self.applicationDocumentsDirectory.appendingPathComponent("SingleViewCoreData.sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
-            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
-        } catch {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject?
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject?
-            
-            dict[NSUnderlyingErrorKey] = error as NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-            abort()
-        }
-        
-        return coordinator
-    }()
-    
-    private lazy var applicationDocumentsDirectory: URL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "ByCyril.OneID" in the application's documents Application Support directory.
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return urls[urls.count-1]
-    }()
-    // MARK: - Core Data Saving support
-    private lazy var managedObjectContext: NSManagedObjectContext = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
-    
-    public func saveContext () {
-        let context = persistentContainer().viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
-    public func saveData<V>(values: [V], keys: [String]) {
-
         let managedContext = managedObjectContext
         let entity = NSEntityDescription.entity(forEntityName: "\(self.entity!)", in: managedContext)
         let item = NSManagedObject(entity: entity!, insertInto: managedContext)
@@ -117,7 +41,7 @@ public class CoreDataModule {
 
     }
     
-    public func sortData(byKey: String, ascending: Bool) -> [NSManagedObject] {
+    public func retrieveAndSort(byKey: String, ascending: Bool) -> [NSManagedObject] {
         
         let managedContext = managedObjectContext
         let fetchrequest = NSFetchRequest<NSFetchRequestResult>(entityName: "\(self.entity!)")
@@ -134,11 +58,8 @@ public class CoreDataModule {
     public func remove(object: [NSManagedObject], index: Int) {
 
         let managedContext = managedObjectContext
-        
         managedContext.delete(object[index])
-        
         try? managedContext.save()
-        
 
     }
 }
